c157f31bc40b186ef331d03639d3ddc34e474c74
monigoncalves <41274448+monigoncalves@users.noreply.github.com>
2018-07-18 00:38:29 -0300

    Add files via upload

---
+++ exday1.py
@@ -0,0 +1,14 @@
+#Exercises Day 1
+
+#1 - Hora em 1 ano
+print(24*365)
+
+#2 - Minutos numa década
+print(24*365*10*60)
+
+#3 - My age in seconds
+print(360*24*365*27)
+
+#4 - Someone aged 48618000s. How old?
+print(48618000/(360*24*365))
+
---
+++ operations.py
@@ -0,0 +1,25 @@
+## Operações Matemáticas
+
+## Potenciação
+print(2**3)
+
+## Modulus (Resto da divisão)
+print(11%2)
+
+## Bitshifting - Números em 32 bits (4 octetos) que são 
+#shiftados para esquerda (preenchimento sempre 0s) ou 
+#direita [preenchimento lógico (0s) ou aritmético (1s)].
+#Notar que: (A) Esquerda: multiplicar por potências de 2; 
+#(B) Direita: dividir por potências de 2. 
+# Ex: 1<<3 ou 1*8; 2>>2 ou 2/4
+#Sintaxe Lógico: >>>
+#Sintaxe Aritmetico: >>  
+
+# Left Shift
+print(1<<9)
+
+# Logical Right Shift
+#print(1>>>9)
+
+#Arithmetic Right Shift
+print(1>>9)
